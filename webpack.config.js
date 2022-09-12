e790a21d6ad1c298b112c256986eb7b7127484dd
cccdev <lujt66@163.com>
2022-09-12 11:29:15 +0800

    chore: ouput config & template html

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,10 @@
 module.exports = {
   entry: './src/index.js',
-  output: { filename: 'bundle.js' },
   mode: 'development',
+  output: {
+    filename: '[name].bundle.js',
+    clean: true,
+  },
   module: {
     rules: [
       {
@@ -13,18 +16,16 @@ module.exports = {
         use: ['style-loader', 'css-loader', 'postcss-loader', 'less-loader'],
       },
       {
-        test: /\.(png|jpe?g|gif)$/i,
-        use: [
-          {
-            loader: 'file-loader',
-            options: {
-              esModule: false, //不转为 esModule
-              name: '[name].[hash:6].[ext]',//按照name+6位hash+扩展名的规则来命名
-              outputPath: 'image', //输出目录
-              limit: 20 * 1024 // 20kb以下才打包成base64格式
-            },
+        test: /\.(png|jpe?g|gif|svg)$/i,
+        type: 'asset',
+        generator: {
+          filename: 'assets/[name].[hash:6][ext]', //输出规则
+        },
+        parser: {
+          dataUrlCondition: {
+            maxSize: 20 * 1024, // 小于20kb则解析成dataUrl
           },
-        ],
+        },
       },
     ],
   },
