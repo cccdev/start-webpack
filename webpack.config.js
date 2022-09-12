472bd052ac64100526a06f66cf513a34dd169c90
cccdev <lujt66@163.com>
2022-09-12 11:20:09 +0800

    chore: add file & url loader

--- webpack.config.js
+++ webpack.config.js
@@ -12,6 +12,20 @@ module.exports = {
         test: /\.less$/i,
         use: ['style-loader', 'css-loader', 'postcss-loader', 'less-loader'],
       },
+      {
+        test: /\.(png|jpe?g|gif)$/i,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              esModule: false, //不转为 esModule
+              name: '[name].[hash:6].[ext]',//按照name+6位hash+扩展名的规则来命名
+              outputPath: 'image', //输出目录
+              limit: 20 * 1024 // 20kb以下才打包成base64格式
+            },
+          },
+        ],
+      },
     ],
   },
 };
