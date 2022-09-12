7d5eafd5625bfe0e36f637650a0e6b4c8aa20d25
cccdev <lujt66@163.com>
2022-09-12 10:33:57 +0800

    feat: init project

---
+++ src/index.js
@@ -0,0 +1,7 @@
+function createElement() {
+  const div = document.createElement('div');
+  div.innerHTML = 'hello world';
+  div.classList.add('div');
+  document.body.append(div);
+}
+createElement();
